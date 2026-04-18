53a2c5b75efaec7e0c2eaf3ebbf67fa3dbc478bc
Yozen <slyiehn@gmail.com>
2026-04-18 12:22:53 +0800

    Added a hardcoded gameplay tag to have a cleaner environment for my tags

---
+++ Source/TestProject/Private/Tags/TPGameplayTags.cpp
@@ -0,0 +1,22 @@
+﻿#include "Tags/TPGameplayTags.h"
+#include "GameplayTagsManager.h"
+
+FTPGameplayTags FTPGameplayTags::GameplayTags;
+
+void FTPGameplayTags::InitializeNativeGameplayTags()
+{
+	GameplayTags.InputTag_1 = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("InputTag.1"), FString("1"));
+	GameplayTags.InputTag_2 = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("InputTag.2"), FString("2"));
+	GameplayTags.InputTag_3 = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("InputTag.3"), FString("3"));
+	GameplayTags.InputTag_4 = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("InputTag.4"), FString("4"));
+	GameplayTags.InputTag_5 = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("InputTag.5"), FString("5"));
+	GameplayTags.InputTag_RMB = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("InputTag.RMB"), FString("RMB"));
+	GameplayTags.InputTag_RMB = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("InputTag.LMB"), FString("LMB"));
+
+	GameplayTags.State_Blocking = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("State.Blocking"), FString("Blocking"));
+	GameplayTags.State_Parrying = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("State.Parrying"), FString("Parrying"));
+
+	GameplayTags.Hit_Critical = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Hit.Critical"), FString("Critical Hit"));
+	
+	GameplayTags.Damage = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Damage"), FString("Damage"));
+}
---
+++ Source/TestProject/Public/Tags/TPGameplayTags.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+struct FTPGameplayTags
+{
+public:
+	static const FTPGameplayTags& Get() { return GameplayTags; }
+	static void InitializeNativeGameplayTags();
+
+	FGameplayTag InputTag_1;
+	FGameplayTag InputTag_2;
+	FGameplayTag InputTag_3;
+	FGameplayTag InputTag_4;
+	FGameplayTag InputTag_5;
+	FGameplayTag InputTag_RMB;
+	FGameplayTag InputTag_LMB;
+
+	FGameplayTag State_Blocking;
+	FGameplayTag State_Parrying;
+
+	FGameplayTag Hit_Critical;
+
+	FGameplayTag Damage;
+	
+private:
+	static FTPGameplayTags GameplayTags;
+};
