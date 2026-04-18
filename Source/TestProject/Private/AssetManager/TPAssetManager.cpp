312b717c9b8f35c670675925916aba7c4c4e8f9b
Yozen <slyiehn@gmail.com>
2026-04-18 11:45:28 +0800

    Added Asset Manager

---
+++ Source/TestProject/Private/AssetManager/TPAssetManager.cpp
@@ -0,0 +1,21 @@
+﻿#include "AssetManager/TPAssetManager.h"
+#include "Tags/TPGameplayTags.h"
+#include "AbilitySystemGlobals.h"
+
+UTPAssetManager& UTPAssetManager::Get()
+{
+	check(GEngine);
+	UTPAssetManager* TPAssetManager = Cast<UTPAssetManager>(GEngine->AssetManager);
+	return *TPAssetManager;
+}
+
+void UTPAssetManager::StartInitialLoading()
+{
+	Super::StartInitialLoading();
+	FTPGameplayTags::InitializeNativeGameplayTags();
+	// ✅ Make sure this is here too!
+	UAbilitySystemGlobals::Get().InitGlobalData();
+}
+
+
+
---
+++ Source/TestProject/Public/AssetManager/TPAssetManager.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetManager.h"
+#include "Component/TPAbilitySystemComponent.h"
+#include "TPAssetManager.generated.h"
+
+UCLASS()
+class TESTPROJECT_API UTPAssetManager : public UAssetManager
+{
+	GENERATED_BODY()
+
+public:
+	static UTPAssetManager& Get();
+
+protected:
+	virtual void StartInitialLoading() override;
+};
